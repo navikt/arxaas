20fdb15befc1b23beebf0e698d3814b800089862
Julian <teritry2@gmail.com>
2019-01-17 13:36:52 +0100

    Demostrated how spring services are used with controller

--- src/main/java/no/OsloMET/AaaS/controller/AnonymizationController.java
+++ src/main/java/no/OsloMET/AaaS/controller/AnonymizationController.java
@@ -1,5 +1,7 @@
 package no.OsloMET.AaaS.controller;
 
+import no.OsloMET.AaaS.service.AnonymizationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class AnonymizationController {
 
+    @Autowired
+    AnonymizationService anonymizationService;
+
     @RequestMapping("/")
     public String presetKAnonymization(){
         System.out.println("Triggered");
+        anonymizationService.sihei();
         return "Successfull";
     }
 }
---
+++ src/main/java/no/OsloMET/AaaS/service/AnonymizationService.java
@@ -0,0 +1,11 @@
+package no.OsloMET.AaaS.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnonymizationService {
+
+    public void sihei(){
+        System.out.println("Hei");
+    }
+}
