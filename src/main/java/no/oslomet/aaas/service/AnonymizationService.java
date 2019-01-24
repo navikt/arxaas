6acbd69e3d65497c6d3ac2d17b69983fae128c3f
andregroseth <andregroseth@hotmail.com>
2019-01-24 15:17:36 +0100

    Create empty ARXWrapper

--- src/main/java/no/oslomet/aaas/service/AnonymizationService.java
+++ src/main/java/no/oslomet/aaas/service/AnonymizationService.java
@@ -1,29 +1,11 @@
 package no.oslomet.aaas.service;
 
-import no.oslomet.aaas.model.Note;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Service
 public class AnonymizationService {
+    AnonymizationService() {
 
-    public Note sihei(){
-        System.out.println("Hei");
-
-        Note note = new Note();
-        note.setDate(new Date(System.currentTimeMillis()));
-        note.setTitle("Hei dette er et spring kurs");
-        note.setMessage("Velkommen til spring");
-
-        List<String> part = new ArrayList<String>();
-        part.add("Jeremiah");
-        part.add("Sondre");
-
-        note.setParticipants(part);
-
-        return note;
     }
 }
+
---
+++ src/main/java/no/oslomet/aaas/utils/ARXWrapper.java
@@ -0,0 +1,5 @@
+package no.oslomet.aaas.utils;
+
+public class ARXWrapper {
+
+}
