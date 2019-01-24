628c3b85e40e6b5ff8087acb53a6e53baa4beb3e
andregroseth <andregroseth@hotmail.com>
2019-01-24 15:07:22 +0100

    Create get method

--- src/main/java/no/oslomet/aaas/controller/AnonymizationController.java
+++ src/main/java/no/oslomet/aaas/controller/AnonymizationController.java
@@ -1,17 +1,14 @@
 package no.oslomet.aaas.controller;
 
 import no.oslomet.aaas.model.AnonymizationPayload;
+import no.oslomet.aaas.model.MetaData;
 import no.oslomet.aaas.service.AnonymizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/anonymize")
 public class AnonymizationController {
-
 
     private AnonymizationService anonymizationService;
 
@@ -20,7 +17,13 @@ public class AnonymizationController {
         this.anonymizationService = anonymizationService;
     }
 
-
+    @GetMapping
+    public AnonymizationPayload anonymization(){
+        AnonymizationPayload payload = new AnonymizationPayload();
+        payload.setData("Viktor");
+        payload.setMetaData(new MetaData());
+        return payload;
+    }
     @PostMapping
     public AnonymizationPayload anonymization(@RequestBody AnonymizationPayload payload){
         payload.setData("Viktor");
