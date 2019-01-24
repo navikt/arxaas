5cfc130791fee71ede5396634be1d1d356129321
JeremiahUy <jery_3000@hotmail.com>
2019-01-24 09:28:35 +0100

    Made a method for jacoco to test

--- src/main/java/no/OsloMET/AaaS/AaaSApplication.java
+++ src/main/java/no/OsloMET/AaaS/AaaSApplication.java
@@ -5,6 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AaaSApplication {
+	
+	//Jacoco plugin test
+	public boolean isPalindrome(String inputString) {
+    		if (inputString.length() == 0) {
+        	return true;
+    		} else {
+			char firstChar = inputString.charAt(0);
+        		char lastChar = inputString.charAt(inputString.length() - 1);
+        		String mid = inputString.substring(1, inputString.length() - 1);
+        		return (firstChar == lastChar) && isPalindrome(mid);
+    		}
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(AaaSApplication.class, args);
