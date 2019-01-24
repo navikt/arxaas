7be8381d1774daf023b41ec35e99a6582c6cb89a
JeremiahUy <jery_3000@hotmail.com>
2019-01-24 10:16:36 +0100

    fixed test method for jacoco

--- src/test/java/no/OsloMET/AaaS/AaaSApplicationTests.java
+++ src/test/java/no/OsloMET/AaaS/AaaSApplicationTests.java
@@ -1,6 +1,7 @@
 package no.OsloMET.AaaS;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,12 +18,12 @@ public class AaaSApplicationTests {
 	@Test
 	public void whenPalindrom_thenAccept() {
     		AaaSApplication palindromeTester = new AaaSApplication();
-    		assertTrue(palindromeTester.isPalindrome("noon"));
+    		Assert.assertTrue(palindromeTester.isPalindrome("noon"));
 	}     
 	@Test
 	public void whenNearPalindrom_thanReject(){
     		AaaSApplication palindromeTester = new AaaSApplication();
-    		assertFalse(palindromeTester.isPalindrome("neon"));
+    		Assert.assertFalse(palindromeTester.isPalindrome("neon"));
 	}
 	//.......................
 }
