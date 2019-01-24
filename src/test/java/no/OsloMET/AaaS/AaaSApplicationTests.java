6274f920666146f27451a069b4442147d0e6ff57
JeremiahUy <jery_3000@hotmail.com>
2019-01-24 09:29:22 +0100

    Jacoco plugin test method

--- src/test/java/no/OsloMET/AaaS/AaaSApplicationTests.java
+++ src/test/java/no/OsloMET/AaaS/AaaSApplicationTests.java
@@ -12,6 +12,12 @@ public class AaaSApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void whenEmptyString_thenAccept() {
+    		Palindrome palindromeTester = new Palindrome();
+    		assertTrue(palindromeTester.isPalindrome(""));
+	}
 
 }
 
