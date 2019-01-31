64acc78f2317bfe75d643d9311e8cd0ff6c8cd42
JeremiahUy <jery_3000@hotmail.com>
2019-01-31 14:45:36 +0100

    UPDATE jacoco pom.xml and travis.yml settings
    configured settings to output a jacoco.xml file

--- src/test/java/no/OsloMET/aaas/AaaSApplicationTest.java
+++ src/test/java/no/OsloMET/aaas/AaaSApplicationTest.java
@@ -1,8 +1,8 @@
 package no.OsloMET.aaas;
 
 import no.oslomet.aaas.AaaSApplication;
-import org.junit.Assert;
-import org.junit.Test;
+        import org.junit.Assert;
+        import org.junit.Test;
 
 public class AaaSApplicationTest {
 
