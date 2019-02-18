40d52daf3e8fedbeb2ea1152bb8ab08518d59c70
Julian <teritry2@gmail.com>
2019-02-18 15:04:08 +0100

    Need the deploy script to run for my tests

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-export IMAGE_NAME=arxaas/aaas
-docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
-docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:latest
-docker push $IMAGE_NAME
 
 docker login -u _json_key -p $gcloud_service_user_json gcr.io
 export IMAGE_NAME=arxaas/aaas
