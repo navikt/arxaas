a1ce066bbcdf2668f9f6bc9ff0da908071f9f354
Julian <julian.sagen@gmail.com>
2019-02-18 16:40:12 +0100

    Updating the docker deploy script so it will use the auth file

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cat google_cloud_api_key.json | docker login -u _json_key --password-stdin gcr.io
-docker login -u _json_key -p $gcloud_service_user_json gcr.io
 export IMAGE_NAME=arxaas/aaas
 docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
 docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:latest
