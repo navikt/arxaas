c070b3015dc430ed65b3b51eac3fa82432e1c002
Julian <teritry2@gmail.com>
2019-02-18 14:36:24 +0100

    Testing google cloud credentials

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -5,3 +5,9 @@ export IMAGE_NAME=arxaas/aaas
 docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
 docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:latest
 docker push $IMAGE_NAME
+
+docker login -u _json_key -p $gcloud_service_user_json gcr.io
+export IMAGE_NAME=arxaas/aaas
+docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
+docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:latest
+docker push $IMAGE_NAME
