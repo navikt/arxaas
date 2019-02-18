1b897b3ed8be09694ebf7128a63d1e7a3c4b0ee5
Julian <julian.sagen@gmail.com>
2019-02-18 16:53:47 +0100

    Had to specify where docker will push the image

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cat google_cloud_api_key.json | docker login -u _json_key --password-stdin gcr.io
+
 export IMAGE_NAME=arxaas/aaas
 docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
 docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:latest
-docker push $IMAGE_NAME
+Docker push gcr.io/carbon-sunup-229608/$IMAGE_NAME
