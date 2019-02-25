2934452ca581428acee626763d3a10578d3fbe65
sondre <sondre.hal@gmail.com>
2019-02-25 12:49:03 +0100

    Make AnonymizationIntegrationTest class public so travis can run test

--- src/test/java/no/oslomet/aaas/controller/AnonymizationIntegrationTest.java
+++ src/test/java/no/oslomet/aaas/controller/AnonymizationIntegrationTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AnonymizationIntegrationTest {
+public class AnonymizationIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
