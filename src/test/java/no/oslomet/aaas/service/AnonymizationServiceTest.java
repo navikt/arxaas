d88fe551c0827be76818c0b50ada78e73a6ac82b
JeremiahUy <jery_3000@hotmail.com>
2019-02-25 16:38:22 +0100

    ADD unit test for Anonymization Service

--- src/test/java/no/oslomet/aaas/service/AnonymizationServiceTest.java
+++ src/test/java/no/oslomet/aaas/service/AnonymizationServiceTest.java
@@ -2,12 +2,10 @@ package no.oslomet.aaas.service;
 
 import no.oslomet.aaas.analyser.ARXAnalyser;
 import no.oslomet.aaas.anonymizer.ARXAnonymiser;
-import no.oslomet.aaas.model.AnonymizationPayload;
-import no.oslomet.aaas.model.MetaData;
-import no.oslomet.aaas.model.PrivacyModel;
-import no.oslomet.aaas.model.SensitivityModel;
+import no.oslomet.aaas.model.*;
 import no.oslomet.aaas.utils.ARXPayloadAnalyser;
 import no.oslomet.aaas.utils.ARXWrapper;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -93,7 +91,59 @@ public class AnonymizationServiceTest {
     }
 
     @Test
-    public void anonymize() {
-        var result = anonymizationService.anonymize(testPayload);
+    public void anonymize_result() {
+        AnonymizationResultPayload test= anonymizationService.anonymize(testPayload);
+        String actual = String.valueOf(test.getAnonymizeResult().getData());
+        String expected = "age,gender,zipcode\n" +
+                "*,male,816**\n" +
+                "*,female,816**\n" +
+                "*,male,816**\n" +
+                "*,female,816**\n" +
+                "*,male,816**\n" +
+                "*,female,816**\n" +
+                "*,male,816**\n" +
+                "*,female,816**\n" +
+                "*,male,816**\n" +
+                "*,female,816**\n" +
+                "*,male,816**\n";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void anonymize_beforeAnonymizationMetrics() {
+        AnonymizationResultPayload test= anonymizationService.anonymize(testPayload);
+        String actual = String.valueOf(test.getBeforeAnonymizationMetrics());
+        String expected = "{measure_value=[%], " +
+                "record_affected_by_highest_risk=100.0, " +
+                "sample_uniques=100.0, estimated_prosecutor_risk=100.0, " +
+                "population_model=ZAYATZ, " +
+                "records_affected_by_lowest_risk=100.0, " +
+                "estimated_marketer_risk=100.0, " +
+                "highest_prosecutor_risk=100.0, " +
+                "estimated_journalist_risk=100.0, " +
+                "lowest_risk=100.0, " +
+                "average_prosecutor_risk=100.0, " +
+                "population_uniques=100.0, " +
+                "quasi_identifiers=[zipcode, gender]}";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void anonymize_afterAnonymizationMetrics() {
+        AnonymizationResultPayload test= anonymizationService.anonymize(testPayload);
+        String actual = String.valueOf(test.getAfterAnonymizationMetrics());
+        String expected = "{measure_value=[%], " +
+                "record_affected_by_highest_risk=45.45454545454545, " +
+                "sample_uniques=0.0, estimated_prosecutor_risk=20.0, " +
+                "population_model=DANKAR, " +
+                "records_affected_by_lowest_risk=54.54545454545454, " +
+                "estimated_marketer_risk=18.181818181818183, " +
+                "highest_prosecutor_risk=20.0, " +
+                "estimated_journalist_risk=20.0, " +
+                "lowest_risk=16.666666666666664, " +
+                "average_prosecutor_risk=18.181818181818183, " +
+                "population_uniques=0.0, " +
+                "quasi_identifiers=[zipcode, gender]}";
+        Assert.assertEquals(expected,actual);
     }
 }
