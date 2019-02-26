f45f47afe3fadc7198eaa130b9c49b16d5982408
JeremiahUy <jery_3000@hotmail.com>
2019-02-26 10:27:04 +0100

    ADD javadoc for AnalysisResult

--- src/main/java/no/oslomet/aaas/model/AnalysisResult.java
+++ src/main/java/no/oslomet/aaas/model/AnalysisResult.java
@@ -2,15 +2,26 @@ package no.oslomet.aaas.model;
 
 import java.util.Map;
 
-
+/**
+ * Model class for Analysis result
+ */
 public class AnalysisResult {
 
     private final Map<String, String> metrics;
 
+    /***
+     * Setter method for analysis result metrics. Fills the object with the results from the analysis against
+     * re-identification risk.
+     * @param metrics HashMap containing fields that describe the type of risk and its value
+     */
     public AnalysisResult(Map<String, String> metrics) {
         this.metrics = metrics;
     }
 
+    /***
+     * Getter method for the analysis result metrics.
+     * @return HashMap containing the results from the analysis against re-identification risk.
+     */
     public Map<String, String> getMetrics() {
         return metrics;
     }
