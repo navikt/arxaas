24ef39bfc723a8ef799f8e34bc03d8b6ba5d4578
vartdalen <viktor.v.johansen@hotmail.com>
2019-02-28 12:03:06 +0100

    CREATE ARXConfigurationSetter and ARXModelSetter classes REFACTOR ownership of methods SetSuppressionLimit, SetPrivacyModels, getPrivacyModel from ARXWrapper to ARXConfigurationSetter REFACTOR ownership of methods SetSensitivityModels (both the anonymization and analyzation version) and setHierarchies from ARXWrapper to ARXModelSetter CREATE dependency injections in ARXWrapper for ARXConfigurationSetter and ARXModelSetter CREATE ARXModelSetterTest and ARXConfigurationSetterTest REFACTOR ownership of test methods from ARXWrapper to ARXModelSetterTest and ARXConfigurationSetterTest

--- src/test/java/no/oslomet/aaas/service/AnalysationServiceTest.java
+++ src/test/java/no/oslomet/aaas/service/AnalysationServiceTest.java
@@ -3,6 +3,8 @@ package no.oslomet.aaas.service;
 import no.oslomet.aaas.analyser.ARXAnalyser;
 import no.oslomet.aaas.model.AnalysationPayload;
 import no.oslomet.aaas.model.AttributeTypeModel;
+import no.oslomet.aaas.utils.ARXConfigurationSetter;
+import no.oslomet.aaas.utils.ARXModelSetter;
 import no.oslomet.aaas.utils.ARXPayloadAnalyser;
 import no.oslomet.aaas.utils.ARXWrapper;
 import org.junit.Assert;
@@ -18,9 +20,10 @@ import static no.oslomet.aaas.model.AttributeTypeModel.QUASIIDENTIFYING;
 public class AnalysationServiceTest {
 
     private AnalysationService analysationService;
-    private ARXWrapper arxWrapper = new ARXWrapper();
+    private ARXWrapper arxWrapper = new ARXWrapper(new ARXConfigurationSetter(), new ARXModelSetter());
+    private ARXModelSetter arxModelSetter = new ARXModelSetter();
     private ARXPayloadAnalyser arxPayloadAnalyser = new ARXPayloadAnalyser();
-    private ARXAnalyser arxAnalyser = new ARXAnalyser(arxWrapper, arxPayloadAnalyser);
+    private ARXAnalyser arxAnalyser = new ARXAnalyser(arxWrapper, arxModelSetter, arxPayloadAnalyser);
 
     @Before
     public void initialize(){ analysationService = new AnalysationService(arxAnalyser); }
