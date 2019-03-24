66acd5e233a7a1a99b4216c2e0c6fe1e918d7f04
sondre <sondre.hal@gmail.com>
2019-03-24 19:00:03 +0100

    ADD Explicit response status on NaisController paths

--- src/main/java/no/oslomet/aaas/controller/NaisController.java
+++ src/main/java/no/oslomet/aaas/controller/NaisController.java
@@ -1,19 +1,23 @@
 package no.oslomet.aaas.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/nais")
 public class NaisController {
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/isAlive")
     public String isAlive() {
         return "OK";
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/isReady")
     public String isReady() {
         return "OK";
