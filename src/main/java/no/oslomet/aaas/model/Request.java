d362bec39ef80bc7cd7d5d1685e8af460fad4c20
sondre <sondre.hal@gmail.com>
2019-03-24 22:22:04 +0100

    ADD basic notNull validation on Request payload

--- src/main/java/no/oslomet/aaas/model/Request.java
+++ src/main/java/no/oslomet/aaas/model/Request.java
@@ -1,10 +1,13 @@
 package no.oslomet.aaas.model;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public class Request {
 
+    @NotNull
     private final List<String[]> data;
+    @NotNull
     private final List<Attribute> attributes;
     private final List<PrivacyModelModel> privacyModels;
 
@@ -14,9 +17,11 @@ public class Request {
         this.privacyModels = privacyModels;
     }
 
+
     public List<String[]> getData() {
         return data;
     }
+
 
     public List<Attribute> getAttributes() {
         return attributes;
