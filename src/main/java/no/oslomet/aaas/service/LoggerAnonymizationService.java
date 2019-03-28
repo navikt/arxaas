35f8cbc1ef6617766a973651f817141ff843573c
andregroseth <andregroseth@hotmail.com>
2019-03-28 11:21:16 +0100

    ADD .gz to gitignore Refractor LoggerAnalyzationnService and LoggerAnonymizationService

--- src/main/java/no/oslomet/aaas/service/LoggerAnonymizationService.java
+++ src/main/java/no/oslomet/aaas/service/LoggerAnonymizationService.java
@@ -1,4 +1,5 @@
 package no.oslomet.aaas.service;
+
 import no.oslomet.aaas.controller.AnonymizationController;
 import no.oslomet.aaas.model.Request;
 import org.slf4j.Logger;
@@ -10,18 +11,18 @@ public class LoggerAnonymizationService {
 
     private Logger anonymizationLogger = LoggerFactory.getLogger(AnonymizationController.class);
 
-    public void loggAnonymizationPayload(Request payload){
-        anonymizationLogger.info("Request received: "+" Size of data set: "+"Number of rows = "+ numRows(payload) +", Number of columns "+ numColumns(payload));
+    public void loggAnonymizationPayload(Request payload) {
+        anonymizationLogger.info("Request received: " + " Size of data set: " + "Number of rows = " + numRows(payload) + ", Number of columns " + numColumns(payload));
     }
 
 
-    private int numColumns(Request payload){
-        if(payload == null || payload.getData() == null) return 0;
-        return  payload.getData().get(0).length;
+    private int numColumns(Request payload) {
+        if (payload == null || payload.getData() == null) return 0;
+        return payload.getData().get(0).length;
     }
 
-    private int numRows(Request payload){
-        if(payload == null || payload.getData() == null) return 0;
+    private int numRows(Request payload) {
+        if (payload == null || payload.getData() == null) return 0;
         return payload.getData().size();
     }
 }
