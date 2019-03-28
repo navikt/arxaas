3c4065788a6d6584298171e14dc9b5edaa0476c3
JeremiahUy <jery_3000@hotmail.com>
2019-03-28 09:57:47 +0100

    REFACTOR split the error message and made a variable of string for them before passing in logger.error() method

--- src/main/java/no/oslomet/aaas/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/no/oslomet/aaas/controller/GlobalControllerExceptionHandler.java
@@ -19,7 +19,7 @@ import java.util.Date;
 @ControllerAdvice
 class GlobalControllerExceptionHandler {
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /**
      * Handles all exceptions thrown unless cached by a more specific handler
@@ -29,7 +29,8 @@ class GlobalControllerExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleExceptionAllExceptions(Exception ex, WebRequest request) {
-        logger.error("Exception.class error, HttpStatus:INTERNAL_SERVER_ERROR "+"ExceptionToString: "+ ex.toString());
+        String error ="Exception.class error, HttpStatus: INTERNAL_SERVER_ERROR, ExceptionToString: "+ ex.toString();
+        logger.error(error);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
                         ex.getMessage(),
                         request.getDescription(false),
@@ -39,7 +40,8 @@ class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public final ResponseEntity<Object> handleMethodNotSupportedExceptions(Exception ex, WebRequest request) {
-        logger.error("Exception error:Exception thrown when a request handler does not support a specific request method. HttpStatus:METHOD_NOT_ALLOWED "+"ExceptionToString: "+ ex.toString());
+        String error = "Exception error:Exception thrown when a request handler does not support a specific request method. HttpStatus: METHOD_NOT_ALLOWED, ExceptionToString: "+ ex.toString();
+        logger.error(error);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
                 ex.getMessage(),
                 request.getDescription(false),
@@ -49,7 +51,8 @@ class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity handleIllegalArgumentExceptions (IllegalArgumentException ex, WebRequest request){
-        logger.error("Exception error:Thrown to indicate that a method has been passed an illegal or inappropriate argument. HttpStatus:BAD_REQUEST "+"ExceptionToString: "+ ex.toString());
+        String error = "Exception error:Thrown to indicate that a method has been passed an illegal or inappropriate argument. HttpStatus: BAD_REQUEST, ExceptionToString: "+ ex.toString();
+        logger.error(error);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
                 ex.getMessage(),
                 request.getDescription(false),
@@ -59,7 +62,8 @@ class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request){
-        logger.error("Exception error:Exception to be thrown when validation on an argument annotated with @Valid fails. HttpStatus:BAD_REQUEST "+"ExceptionToString: "+ex.toString());
+        String error = "Exception error:Exception to be thrown when validation on an argument annotated with @Valid fails. HttpStatus: BAD_REQUEST, ExceptionToString: "+ex.toString();
+        logger.error(error);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
                 ex.getMessage(),
                 request.getDescription(false),
