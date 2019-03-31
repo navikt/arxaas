320cc8b4847ac9eb1161dcf4aef60589c29bffcb
Julian <teritry2@gmail.com>
2019-04-01 01:01:18 +0200

    Split the gathering of data into multiple smaller static methods
    Also wrote some javadoc

--- src/main/java/no/oslomet/aaas/model/AnonymizationMetrics.java
+++ src/main/java/no/oslomet/aaas/model/AnonymizationMetrics.java
@@ -6,19 +6,68 @@ import org.deidentifier.arx.ARXResult;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * Model class for displaying metrics from the anonymization process
+ */
 public class AnonymizationMetrics {
 
-    private List<AttributeGeneralizationRow> attributeGeneralization = new ArrayList<AttributeGeneralizationRow>();
+    private List<AttributeGeneralizationRow> attributeGeneralization;
+    private Long processTimeMillisecounds;
+    private Set privacyModels;
 
-    public AnonymizationMetrics(ARXResult result){
-        ARXLattice.ARXNode node = result.getOutput().getTransformation();
-        for(String attribute : node.getQuasiIdentifyingAttributes()) {
-            attributeGeneralization.add(new AttributeGeneralizationRow(result.getOutput().getDefinition().getAttributeType(attribute).toString(), node.getGeneralization(attribute)));
-        }
+    /**
+     * Constructor for populating the class with data from a {@link ARXResult} object
+     * @param result Incoming {@link ARXResult} containing the data from a completed anonymizaton
+     */
+    public AnonymizationMetrics(ARXResult result) {
+        attributeGeneralization = gatherGeneralizationAttributes(result);
+        processTimeMillisecounds = gatherProcessTime(result);
+        privacyModels = gatherPrivacyModels(result);
     }
 
-    public List<AttributeGeneralizationRow> getAttributeRows() {
+    /**
+     * Gathers the name, types and generalization level for each attribute and returns them in the form of a {@link AttributeGeneralizationRow}
+     * @param result source where the data is gathered from
+     * @return List of {@link AttributeGeneralizationRow}'s
+     */
+    static private List<AttributeGeneralizationRow> gatherGeneralizationAttributes(ARXResult result) {
+        List<AttributeGeneralizationRow> attributeGeneralizationList = new ArrayList<AttributeGeneralizationRow>();
+        ARXLattice.ARXNode node = result.getOutput().getTransformation();
+        for (String attribute : node.getQuasiIdentifyingAttributes()) {
+            attributeGeneralizationList.add(new AttributeGeneralizationRow(attribute, result.getOutput().getDefinition().getAttributeType(attribute).toString(), node.getGeneralization(attribute)));
+        }
+        return attributeGeneralizationList;
+    }
+
+    /**
+     * Gathers the elapsed time the anonymization process took in milliseconds
+     * @param result Source {@link ARXResult} which the data is gathered from
+     * @return Time the anonymization process have taken in milliseconds
+     */
+    static private long gatherProcessTime(ARXResult result) {
+        return result.getTime();
+    }
+
+    /**
+     * Gathers set of privacymodel data from result object
+     * @param result Source {@link ARXResult} which the data is gathered from
+     * @return Set with data from the privacy model settings currently being applied to the anonymization process
+     */
+    private Set gatherPrivacyModels(ARXResult result) {
+        return result.getConfiguration().getPrivacyModels();
+    }
+
+    public List<AttributeGeneralizationRow> getAttributeGeneralization() {
         return attributeGeneralization;
+    }
+
+    private Long getProcessTimeMillisecounds() {
+        return processTimeMillisecounds;
+    }
+
+    public Set getPrivacyModels() {
+        return privacyModels;
     }
 }
--- src/main/java/no/oslomet/aaas/model/AttributeGeneralizationRow.java
+++ src/main/java/no/oslomet/aaas/model/AttributeGeneralizationRow.java
@@ -2,10 +2,12 @@ package no.oslomet.aaas.model;
 
 public class AttributeGeneralizationRow {
     final String name;
+    final String type;
     final int generalizationLevel;
 
-    public AttributeGeneralizationRow(String name, int generalizationLevel){
+    public AttributeGeneralizationRow(String name, String type, int generalizationLevel){
         this.name = name;
+        this.type = type;
         this.generalizationLevel = generalizationLevel;
     }
 
@@ -17,4 +19,7 @@ public class AttributeGeneralizationRow {
         return generalizationLevel;
     }
 
+    public String getType() {
+        return type;
+    }
 }
