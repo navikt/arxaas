6b3ec1ad7a3f7041084b0d3370cc5bed8b8a5841
Julian <teritry2@gmail.com>
2019-04-03 09:29:01 +0200

    Enables cross origin in order to allow javascript clients to fetch from our api

--- src/main/java/no/oslomet/aaas/controller/AnalyzationController.java
+++ src/main/java/no/oslomet/aaas/controller/AnalyzationController.java
@@ -5,15 +5,12 @@ import no.oslomet.aaas.model.Request;
 import no.oslomet.aaas.service.AnalyzationService;
 import no.oslomet.aaas.service.LoggerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/analyze")
 public class AnalyzationController {
