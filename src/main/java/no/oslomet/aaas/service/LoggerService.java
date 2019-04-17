d7a64472c5c66d214e85dfea93667b26f1e3c789
andregroseth <andregroseth@hotmail.com>
2019-04-17 11:43:14 +0200

    ADD Anonymization request complete time

--- src/main/java/no/oslomet/aaas/service/LoggerService.java
+++ src/main/java/no/oslomet/aaas/service/LoggerService.java
@@ -1,5 +1,6 @@
 package no.oslomet.aaas.service;
 
+import no.oslomet.aaas.model.AnonymizationResultPayload;
 import no.oslomet.aaas.model.Request;
 import no.oslomet.aaas.model.analytics.RiskProfile;
 import org.slf4j.Logger;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoggerService {
 
-    public void loggPayload(Request payload, String IP, Class classToLogg) {
+    public void loggPayload(Request payload, String ip, Class classToLogg) {
         Logger logger = LoggerFactory.getLogger(classToLogg);
-        logger.info("Request received, " + "Size of data set: " + "Number of rows = " + numRows(payload) + ", Number of columns " + numColumns(payload) + ", Bytesize = " + bytesize(payload) +", Request Source IP = " + IP);
+        logger.info("Request received, " + "Size of data set: " + "Number of rows = " + numRows(payload) + ", Number of columns " + numColumns(payload) + ", Bytesize = " + bytesize(payload) +", Request Source IP = " + ip);
     }
 
     public void loggAnalyzationResult(RiskProfile analyzationResult,long requestProcessingTime, Class classToLogg) {
@@ -19,7 +20,10 @@ public class LoggerService {
         logger.info("Request complete, " + " Request processing time = " + requestProcessingTime +" milliseconds");
     }
 
-
+    public void loggAnonymizeResult(AnonymizationResultPayload payload, long requestProcessingTime, Class classToLogg, String ip) {
+        Logger logger = LoggerFactory.getLogger(classToLogg);
+        logger.info("Request complete, " + "Size of data set: " + "Number of rows = " + numRows(payload) + ", Number of columns " + numColumns(payload) + ", Bytesize = " + bytesize(payload) +", Request Source IP = " + ip + " Request processing time = " + requestProcessingTime +" milliseconds");
+    }
 
 
     private int numColumns(Request payload) {
@@ -36,4 +40,22 @@ public class LoggerService {
         if (payload == null || payload.getData() == null) return 0;
         return payload.getData().toString().length();
     }
+
+
+    private int numColumns(AnonymizationResultPayload payload) {
+        if (payload == null || payload.getAnonymizeResult() == null || payload.getAnonymizeResult().getData() == null) return 0;
+        return payload.getAnonymizeResult().getData().get(0).length;
+    }
+
+    private int numRows(AnonymizationResultPayload payload) {
+        if (payload == null || payload.getAnonymizeResult() == null || payload.getAnonymizeResult().getData() == null) return 0;
+        return payload.getAnonymizeResult().getData().size();
+    }
+
+    private int bytesize(AnonymizationResultPayload payload) {
+        if (payload == null || payload.getAnonymizeResult() == null || payload.getAnonymizeResult().getData() == null) return 0;
+        return payload.getAnonymizeResult().getData().toString().length();
+    }
+
+
 }
