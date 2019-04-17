81446592b311832a74f995061d2b234882a0e3e3
andregroseth <andregroseth@hotmail.com>
2019-04-17 14:32:26 +0200

    ADD Analyzation  dataset metrics to log

--- src/main/java/no/oslomet/aaas/service/LoggerService.java
+++ src/main/java/no/oslomet/aaas/service/LoggerService.java
@@ -15,9 +15,9 @@ public class LoggerService {
         logger.info("Request received, " + "Size of data set: " + "Number of rows = " + numRows(payload) + ", Number of columns " + numColumns(payload) + ", Bytesize = " + bytesize(payload) + ", Request Source IP = " + ip);
     }
 
-    public void loggAnalyzationResult(RiskProfile analyzationResult, long requestProcessingTime, Class classToLogg) {
+    public void loggAnalyzationResult(RiskProfile analyzationResult, Request payload, String ip, long requestProcessingTime, Class classToLogg) {
         Logger logger = LoggerFactory.getLogger(classToLogg);
-        logger.info("Request complete, " + " Request processing time = " + requestProcessingTime + " milliseconds");
+        logger.info("Request complete, " + "Size of data set: " + "Number of rows = " + numRows(payload) + ", Number of columns " + numColumns(payload) + ", Bytesize = " + bytesize(payload) + ", Request Source IP = " + ip + " Request processing time = " + requestProcessingTime + " milliseconds");
     }
 
     public void loggAnonymizeResult(AnonymizationResultPayload payload, long requestProcessingTime, Class classToLogg, String ip) {
