197e31b0e5083525d58b1f63082f7fa9b62d98f3
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-07-09 14:31:47 +0200

    REMOVE unused config and build files
    REMOVE docker deploy file reference in deploy.sh

--- docker_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-export IMAGE_NAME=arxaas/aaas
-docker build -t $IMAGE_NAME:$TRAVIS_COMMIT .
-docker tag $IMAGE_NAME:$TRAVIS_COMMIT $IMAGE_NAME:latest
-docker push $IMAGE_NAME
