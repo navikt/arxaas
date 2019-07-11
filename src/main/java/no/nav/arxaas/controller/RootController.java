3bcd4bf4ba092b957c53275fea22c7f85b3d8dc3
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-07-11 18:14:07 +0200

    Update RootController to serve static index.html page

--- src/main/java/no/nav/arxaas/controller/RootController.java
+++ src/main/java/no/nav/arxaas/controller/RootController.java
@@ -1,33 +1,25 @@
 package no.nav.arxaas.controller;
 
 
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.Resources;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * Controller serving available resources for the service
  */
-@RestController()
+@Controller
 @RequestMapping(path = "/")
 public class RootController {
 
-    /**
-     * @return Resources object containing links to available resources in the service
-     */
-    @GetMapping
-    public Resources<Object> root(){
-        Link link_root = linkTo(this.getClass()).withSelfRel();
-        Link link_anonymize = linkTo(AnonymizationController.class).withRel("anonymize");
-        Link link_analyze = linkTo(AnalyzationController.class).withRel("analyze");
-        return new Resources<>(Collections.emptySet(), List.of(link_root, link_anonymize, link_analyze));
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:index.html";
     }
 
 }
