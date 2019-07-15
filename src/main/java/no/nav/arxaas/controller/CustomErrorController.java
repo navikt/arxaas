3ece227c0f3bcc8ca82fa339bd9c116a80f1c199
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-07-15 15:19:56 +0200

    UPDATE CustomErrorController to return the standard exception JSON format

--- src/main/java/no/nav/arxaas/controller/CustomErrorController.java
+++ src/main/java/no/nav/arxaas/controller/CustomErrorController.java
@@ -1,32 +1,28 @@
 package no.nav.arxaas.controller;
 
+import no.nav.arxaas.exception.ExceptionResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    public ResponseEntity<ExceptionResponse> handleError(HttpServletRequest request) {
+        Integer status = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
+        ExceptionResponse exceptionResponse = ExceptionResponse.now(status.toString(), request.getServletPath());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.valueOf(status));
 
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "404 Not Found";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "500 Internal Server Error";
-            }
-        }
-        return "Something went wrong";
     }
 
     @Override
