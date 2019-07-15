2e82cdc989e6583b3d9d460a7eb133a725e11332
Julian Sagen <juliansagen@Julians-MacBook-Pro.local>
2019-07-15 13:09:09 +0200

    Minor cleanup

--- src/main/java/no/nav/arxaas/controller/CustomErrorController.java
+++ src/main/java/no/nav/arxaas/controller/CustomErrorController.java
@@ -2,7 +2,6 @@ package no.nav.arxaas.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
