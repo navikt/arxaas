d97a248b832fc1033b600c3d02251770f09a449b
Julian Sagen <juliansagen@Julians-MacBook-Pro.local>
2019-07-15 13:08:47 +0200

    Minor cleanup

--- src/main/java/no/nav/arxaas/controller/CustomErrorController.java
+++ src/main/java/no/nav/arxaas/controller/CustomErrorController.java
@@ -13,8 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/")
 public class CustomErrorController implements ErrorController {
 
-
-
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
