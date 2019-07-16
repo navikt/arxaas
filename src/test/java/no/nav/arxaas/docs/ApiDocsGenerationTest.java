6213a139702ee4c826c1d298491ba5b1706edcc1
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-07-16 17:22:55 +0200

    ADD Date hierarchy builder to API docs

--- src/test/java/no/nav/arxaas/docs/ApiDocsGenerationTest.java
+++ src/test/java/no/nav/arxaas/docs/ApiDocsGenerationTest.java
@@ -2,10 +2,7 @@ package no.nav.arxaas.docs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.arxaas.GenerateTestData;
-import no.nav.arxaas.hierarchy.HierarchyRequest;
-import no.nav.arxaas.hierarchy.Level;
-import no.nav.arxaas.hierarchy.OrderBasedHierarchyBuilder;
-import no.nav.arxaas.hierarchy.RedactionBasedHierarchyBuilder;
+import no.nav.arxaas.hierarchy.*;
 import no.nav.arxaas.model.Request;
 
 import no.nav.arxaas.hierarchy.RedactionBasedHierarchyBuilder.Order;
@@ -238,6 +235,37 @@ class ApiDocsGenerationTest {
                                 subsectionWithPath("builder.levels[].groups[].grouping").description("Number of items to be grouped from the dataset column values"),
                                 subsectionWithPath("builder.levels[].groups[].label").description("Optional label to replace the default generalized value")
                         )));
+    }
+
+    @Test
+    void hierarchy_date() throws Exception {
+        var dateColumn = new String[]{"2020-07-16 15:28:024", "2019-07-16 16:38:025", "2019-07-16 17:48:025", "2019-07-16 18:48:025", "2019-06-16 19:48:025", "2019-06-16 20:48:025"};
+
+        String stringDateFormat = "yyyy-MM-dd HH:mm:SSS";
+
+        var granularities = List.of(
+                DateBasedHierarchyBuilder.Granularity.SECOND_MINUTE_HOUR_DAY_MONTH_YEAR,
+                DateBasedHierarchyBuilder.Granularity.MINUTE_HOUR_DAY_MONTH_YEAR,
+                DateBasedHierarchyBuilder.Granularity.HOUR_DAY_MONTH_YEAR,
+                DateBasedHierarchyBuilder.Granularity.DAY_MONTH_YEAR);
+
+        DateBasedHierarchyBuilder builder = new DateBasedHierarchyBuilder(stringDateFormat, granularities);
+        HierarchyRequest intervalHierarchyRequest = new HierarchyRequest(dateColumn, builder);
+        ObjectMapper mapper = new ObjectMapper();
+        String req = mapper.writeValueAsString(intervalHierarchyRequest);
+
+
+        this.mockMvc.perform(post("/api/hierarchy")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(req))
+                .andExpect(status().isOk())
+                .andDo(document("hierarchy-controller-datebased", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                        requestFields(
+                                subsectionWithPath("column").description("List of values to create the hierarchy for"),
+                                subsectionWithPath("builder").description("Object containing the different parameters on how to build the heirarchy for the dataset column"),
+                                subsectionWithPath("builder.type").description("Hierarchy builder type to use when creating the hierarchy"),
+                                subsectionWithPath("builder.granularities").description("List of Date granularities to create the hierarchy after"),
+                                subsectionWithPath("builder.dateFormat").description("SimpleDateFormat string describing how the date values should be parsed"))));
     }
 
     private static String[] getExampleData(){
